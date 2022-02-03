168f8c994ee31d8fc336b89c4432c48e2e553990
Hyunseok Eddie Lee <hyunseok.eddie.lee@gmail.com>
2022-02-03 17:47:09 -0600

    first commit

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, session, redirect, request
+
+app = Flask(__name__)
+app.secret_key="Benny bob wuz heer."
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/process',methods=['POST'])
+def process():
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comments'] = request.form['comments']
+    return redirect('/results')
+
+@app.route('/results')
+def results():
+    return render_template('results.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
